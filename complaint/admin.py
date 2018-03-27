d1b9b52a2514727187fa9e26f59b0d1be5686b26
vidya <bhartividya0421@gmail.com>
2018-03-27 11:51:17 +0530

    all okay

--- complaint/admin.py
+++ complaint/admin.py
@@ -2,8 +2,4 @@ from django.contrib import admin
 from .models import*
 # Register your models here.
 admin.site.register(student)
-admin.site.register(st_16CA6001)
 admin.site.register(dummy)
-admin.site.register(st_16CA6023)
-admin.site.register(st_16CA6004)
-admin.site.register(st_16CA6032)
